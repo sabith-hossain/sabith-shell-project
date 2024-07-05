df0231b4bea171ff03cb00f18be0e7c2bcfd9a73
Sabith Hossain <85215110+sabith-hossain@users.noreply.github.com>
2024-07-05 17:17:15 +0300

    Add files via upload

---
+++ aws_resource_traker.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#########################
+# Author: Sabith
+# Date: 24th-jun-2024
+#
+# Version: v1
+#
+# This Script will report the aws resource usage
+#########################
+
+# Tracking featers R
+# AWS S3
+# AWS EC2
+# AWS Lambda
+# AWS IAM Users
+# #############
+
+
+# list s3 buckets
+aws s3 list
+
+# list EC2 Instances
+aws ec2 describe-instances
+
+# list Lamda Functions
+aws lambda list-functions
+
+# list IAM users
+aws iam list-users
+
+
